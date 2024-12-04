49e7cec3929bbe8ad05d22b6158b92649ce0c94a
Sanjay <sanjay.k@affinidi.com>
2024-12-04 12:40:16 +0530

    fix: java version

---
+++ credential-issuance/src/main/kotlin/credentialissuance/CredentialIssuanceApplication.kt
@@ -0,0 +1,11 @@
+package credentialissuance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CredentialIssuanceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CredentialIssuanceApplication>(*args)
+}
